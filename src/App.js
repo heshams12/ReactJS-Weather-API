a9e36340324f2e32a2d46c5f1a89943a94af40ee
hesham sobh <Heshams12@hotmail.com>
2018-08-05 17:19:53 -0700

    updated security

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const API_CALL = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
+    const API_CALL = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
     const data = await API_CALL.json();
     if(city && country) {
     //gets the data from API in the object array.
